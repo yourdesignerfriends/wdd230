9f4d8a789db7e0344f9aaffffe594ea405b27da4
[yourdesignerfriends] <[analinadelvecchio@gmail.com]>
2025-03-11 14:56:41 -0700

    fix problems

--- chamber/scripts/hamburgerMenu.js
+++ chamber/scripts/hamburgerMenu.js
@@ -1,11 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     const hamButton = document.querySelector('#menu');
     const navigation = document.querySelector('.navigation');
-    const mainTitle = document.querySelector('#main-title');
 
     hamButton.addEventListener('click', () => {
         navigation.classList.toggle('open');
         hamButton.classList.toggle('open');
-        mainTitle.classList.toggle('hidden');
     });
 });
