fba4e3ee186243cac6683728e5e780e5192b48d2
[yourdesignerfriends] <[analinadelvecchio@gmail.com]>
2025-03-12 16:28:04 -0700

    css footer

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -2,14 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const currentYear = new Date().getFullYear(); 
     const lastModified = document.lastModified; 
     const site = "Oly Chamber of Commerce";
-    const author = "Analina Del Vecchio";
-    const project = "BYU Idaho | WDD 230 Project";
 
     const copyrightElement = document.getElementById("copyright"); 
     const modifiedElement = document.getElementById("lastModified");
     
     if (copyrightElement) { 
-        copyrightElement.textContent = `\u00A9 ${currentYear} ${site} | ${author} | ${project}`; 
+        copyrightElement.textContent = `\u00A9 ${currentYear} ${site}. All rights reserved.`; 
         } 
     if (modifiedElement) { 
         modifiedElement.textContent = `Last Modified: ${lastModified}`; 
