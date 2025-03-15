b9dacc26e769c1eb6777b09a936049576868cf6a
[yourdesignerfriends] <[analinadelvecchio@gmail.com]>
2025-03-14 17:25:21 -0700

    hamburguer menu

--- scripts/activeNavLink.js
+++ scripts/activeNavLink.js
@@ -1,11 +1,13 @@
-const currentPage = window.location.pathname.split("/").pop();
+document.addEventListener('DOMContentLoaded', () => {
+    const currentPage = window.location.pathname.split("/").pop();
 
-const navLinks = document.querySelectorAll("nav a");
+    const navLinks = document.querySelectorAll("nav a");
 
-navLinks.forEach(link => {
-    if (link.getAttribute("href") === currentPage) {
-        link.classList.add("active");
-    } else {
-        link.classList.remove("active");
-    }
+    navLinks.forEach(link => {
+        if (link.getAttribute("href") === currentPage) {
+            link.classList.add("active");
+        } else {
+            link.classList.remove("active");
+        }
+    });
 });
---
+++ scripts/hamburgerMenu.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hamButton = document.querySelector('#menu');
+    const navigation = document.querySelector('.navigation');
+
+    hamButton.addEventListener('click', () => {
+        navigation.classList.toggle('open');
+        hamButton.classList.toggle('open');
+    });
+});
