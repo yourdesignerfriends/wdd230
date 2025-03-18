62a416f1fb396c3a03eb18fa869e23bab9dc8a67
[yourdesignerfriends] <[analinadelvecchio@gmail.com]>
2025-03-17 21:50:43 -0700

    create dark mode

--- chamber/scripts/darkMode.js
+++ chamber/scripts/darkMode.js
@@ -3,14 +3,29 @@ const modeButton = document.querySelector("#darkBtn");
 modeButton.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
 
-    // Toggle the SVG icon
     if (document.body.classList.contains("dark-mode")) {
         modeButton.src = "images/light.svg";
         modeButton.alt = "Switch to light mode";
         modeButton.setAttribute("aria-label", "Activate light mode");
+
+        document.querySelector('header img').src = "images/logo-dark.webp";
+        document.querySelector('header img').alt = "Logo in dark mode";
+
+        document.querySelector('[aria-label="facebook"] img').src = "images/facebook-dark.svg";
+        document.querySelector('[aria-label="instagram"] img').src = "images/instagram-dark.svg";
+        document.querySelector('[aria-label="linkedin"] img').src = "images/linkedin-dark.svg";
+        document.querySelector('[aria-label="youtube"] img').src = "images/youtube-dark.svg";
     } else {
         modeButton.src = "images/dark.svg";
         modeButton.alt = "Switch to dark mode";
         modeButton.setAttribute("aria-label", "Activate dark mode");
+
+        document.querySelector('header img').src = "images/logo-3.webp";
+        document.querySelector('header img').alt = "Logo";
+
+        document.querySelector('[aria-label="facebook"] img').src = "images/facebook.svg";
+        document.querySelector('[aria-label="instagram"] img').src = "images/instagram.svg";
+        document.querySelector('[aria-label="linkedin"] img').src = "images/linkedin.svg";
+        document.querySelector('[aria-label="youtube"] img').src = "images/youtube.svg";
     }
 });
