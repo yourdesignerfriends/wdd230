56c6547fe29f2a725f538f4930012cdc0862bc96
[yourdesignerfriends] <[analinadelvecchio@gmail.com]>
2025-03-17 18:33:40 -0700

    change

---
+++ chamber/scripts/darkMode.js
@@ -0,0 +1,16 @@
+const modeButton = document.querySelector("#darkBtn");
+
+modeButton.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    // Toggle the SVG icon
+    if (document.body.classList.contains("dark-mode")) {
+        modeButton.src = "images/light.svg";
+        modeButton.alt = "Switch to light mode";
+        modeButton.setAttribute("aria-label", "Activate light mode");
+    } else {
+        modeButton.src = "images/dark.svg";
+        modeButton.alt = "Switch to dark mode";
+        modeButton.setAttribute("aria-label", "Activate dark mode");
+    }
+});
