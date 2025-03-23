79ce9921f2726a3487792b74c143c0cf638d7699
[yourdesignerfriends] <[analinadelvecchio@gmail.com]>
2025-03-22 21:10:26 -0700

    add fade in img

---
+++ chamber/scripts/fadeIn.js
@@ -0,0 +1,11 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+        }
+    });
+});
+
+document.querySelectorAll('.fade-in').forEach(element => {
+    observer.observe(element);
+});
