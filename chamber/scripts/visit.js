d876e2ef4991c3a36b2246b09f2b7d3fd4078ef3
[yourdesignerfriends] <[analinadelvecchio@gmail.com]>
2025-03-24 13:58:31 -0700

    use date now

--- chamber/scripts/visit.js
+++ chamber/scripts/visit.js
@@ -5,18 +5,20 @@ let lastVisit = localStorage.getItem("last-check");
 if (!lastVisit) {
     visitsDisplay.textContent = "Welcome! Let us know if you have any questions.";
 } else {
-    const currentDate = new Date();
-    const previousVisitDate = new Date(lastVisit);
-    const timeDifference = currentDate - previousVisitDate;
+    const previousVisitTime = parseInt(lastVisit, 10);
+    const currentTime = Date.now();
+
+    const timeDifference = currentTime - previousVisitTime;
     const oneDay = 24 * 60 * 60 * 1000;
+
+    const daysBetweenVisits = Math.floor(timeDifference / oneDay);
+    const dayText = daysBetweenVisits === 1 ? "day" : "days";
 
     if (timeDifference < oneDay) {
         visitsDisplay.textContent = "Back so soon! Awesome!";
     } else {
-        const daysBetweenVisits = Math.floor(timeDifference / oneDay);
-        const dayText = daysBetweenVisits === 1 ? "day" : "days";
         visitsDisplay.textContent = "You last checked in " + daysBetweenVisits + " " + dayText + " ago.";
     }
 }
 
-localStorage.setItem("last-check", new Date().toString());
+localStorage.setItem("last-check", Date.now());
