2aa442144f16eef62cc43aea82ae5f742d27fc67
[yourdesignerfriends] <[analinadelvecchio@gmail.com]>
2025-03-31 14:12:38 -0700

    making progress

---
+++ chamber/scripts/join.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const timestampField = document.getElementById("form-timestamp");
+    const currentTimestamp = new Date().toISOString();
+    timestampField.value = currentTimestamp;
+});  
