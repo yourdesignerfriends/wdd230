abe18137a485978c19d66eabba8517e783e7a1c9
[yourdesignerfriends] <[analinadelvecchio@gmail.com]>
2025-04-01 13:13:21 -0700

    activity 4 fisnish

--- week05/scripts/weather.js
+++ week05/scripts/weather.js
@@ -2,32 +2,32 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = ''; 
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.74981176612802&lon=6.65889727700159&units=imperial&appid=375385acfd2acb8edc3fe91e2228d658';
 
 async function apiFetch() {
-  try {
-    const response = await fetch(url);
-    if (response.ok) {
-      const data = await response.json();
-      console.log(data);
-      displayResults(data);
-    } else {
-      throw Error(await response.text());
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        displayResults(data);
+        } else {
+        throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
     }
-  } catch (error) {
-    console.log(error);
-  }
 }
 
 apiFetch();
 
 function displayResults(data) {
-  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
-  const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-  let desc = data.weather[0].description;
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    let desc = data.weather[0].description;
 
-  weatherIcon.setAttribute('src', iconsrc);
-  weatherIcon.setAttribute('alt', desc);
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
 
-  captionDesc.textContent = desc;
+    captionDesc.textContent = desc;
 }
